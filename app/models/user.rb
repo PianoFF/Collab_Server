d3d98397e6f1c4014501cbd00e0dd4d30bfe1d50
PianoFF <Fangfei1star@gmail.com>
2020-01-29 20:32:40 +0000

    Message & message <=> user association

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,10 @@ class User < ApplicationRecord
   has_one :vocal
   has_one :instrumental
 
-  
+  has_many :sent_messages, class_name: "Message", foreign_key: "sender_id"
+  has_many :received_messages, class_name: "message", foreign_key: "recipient_id"
+
+
 
 
 end
