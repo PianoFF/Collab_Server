fca889e2bb529ec165b1fa6b4533ae81ab0eb948
PianoFF <Fangfei1star@gmail.com>
2020-01-29 21:15:23 +0000

    User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,14 @@ class User < ApplicationRecord
 
   has_many :sent_messages, class_name: "Message", foreign_key: "sender_id"
   has_many :received_messages, class_name: "Message", foreign_key: "recipient_id"
-
+ 
+  validates :first_name, :last_name, :email,  presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP}
+  validates :field, inclusion: { in: %w(instrumentalist vocalist),
+    message: "%{value} is not a valid option" }
+  validates :bio_content, length: { maximum: 500}
+  validates :password, length: { in: 6..20 }
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :messages
-  resources :instrumentals
-  resources :vocals
-  resources :posts
-  resources :locations
-  resources :users
+  # resources :messages
+  # resources :instrumentals
+  # resources :vocals
+  # resources :posts
+  # resources :locations
+  # resources :users
+
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
